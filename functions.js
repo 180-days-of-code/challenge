af2fd62fb3f4055a9b8ee5d50856246976747a5f
bobbidigi <bobbidigi34@gmail.com>
2017-08-15 05:15:08 -0400

    dynamic table data

--- functions.js
+++ functions.js
@@ -2,3 +2,20 @@ $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
+
+var teams =["Wildcards","The Coders United","JAVASK","Web Pros"];
+
+var table = document.createElement('table');
+document.getElementById('container').appendChild(table);
+for (row = 0; row < 5; row++) {
+    tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (cell = 0; cell < 5; cell++) {
+        td = document.createElement('td');
+        tr.appendChild(td);
+        td.innerHTML = "text";
+    }
+}
+
+	
+	
