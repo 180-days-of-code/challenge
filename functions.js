10c1876880b11e1f639c47bc2a13d2eeee716fe9
bobbidigi <bobbidigi34@gmail.com>
2017-08-21 06:45:30 -0400

    home button still needs better styling

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
-
+$(function(){
+  $("#home-button").on({
+   mouseenter: function(){
+    $(this).attr('src','http://www.clker.com/cliparts/d/3/R/P/q/e/hot-pink-home-icon-md.png');
+  },
+  mouseleave: function(){
+    $(this).attr('src','http://openclipart.org/image/800px/svg_to_png/17103/claudita_home_icon.png');
+  }
+  });
+  
+});
 /*$(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
