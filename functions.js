501b26bfe7c57b4e3f59339251f4c6f24a5aa26f
bobbidigi <bobbidigi34@gmail.com>
2017-08-22 09:33:38 -0400

    ... organizing fixing links

--- functions.js
+++ functions.js
@@ -27,41 +27,5 @@ for (row = 0; row < teams.length; row++) {
         td.innerHTML = "text";
     }
 }*/
-var current = '<a href="#"><p>portfolio</p></a>';
-var next = '<a href="https://www.facebook.com/groups/1889829428005992/permalink/1894852707503664/"><p>week 2</p></a>';
-var greatJob = '<p class="success">Great job! 100%</p>';
-var dataSet = [
-	[ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>MEAN Team</p>", current, next, greatJob],
-	[ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>CodeBeast</p>", current, next, greatJob],
-	[ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Creative Dev</p>", current, next, greatJob],
-	[ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>DevLogics</p>", current, next, greatJob],
-	[ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Diverse Dev Ops</p>", current, next, greatJob],
-    [ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Wild Cards</p>", current, next, greatJob],
-    [  "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Coders United</p>", current, next, greatJob],
-    [  "<img src='img/180-days.jpg' class='leader-img'/>", "<p>JAVASK</p>", current, next, greatJob],
-    [  "<img src='img/180-days.jpg' class='leader-img'/>", "<p>HacknPull</p>", current, next, greatJob ],
-    ["<img src='img/180-days.jpg' class='leader-img'/>", "<p>Web Pros</p>", current, next, greatJob ],
-    [ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>WebDev Squad</p>", current, next, greatJob ],
-    [ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>DigiSprouts</p>", current, next, greatJob ],
-    [ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Game of Codes</p>", current, next, greatJob ],
-    [ "<img src='img/180-days.jpg' class='leader-img'/>", "<p>Evolve-Code</p>", current, next, greatJob ]
-   
-];
- 
-$(document).ready(function() {
-	
-    $('#example').DataTable( {
-		responsive: true,
-        data: dataSet,
-        columns: [
-            { title: "Name" },
-            { title: "Team" },
-            { title: "Current project" },
-            { title: "Next project" },
-            { title: "Status" }
-        ]
-    } );
-	
-} );
 
   
