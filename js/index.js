742cc63254b26630629fbdcc54ceeccaffc96beb
Ritik <patniritik@gmail.com>
2017-09-02 02:10:48 +0530

    feat:added switching on click in resources page

--- js/index.js
+++ js/index.js
@@ -1,2 +1,36 @@
 $(".button-collapse").sideNav();
 $(".dropdown-button").dropdown();
+$("#all").click(function() {
+    $(".inspirationsCards").children("div").each(function() {
+        $(this).css("display", "");
+    });
+});
+
+$("#portfolio").click(function() {
+    $(".inspirationsCards").children("div").each(function() {
+        if ($(this).hasClass("portfolio")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+
+$("#tribute").click(function() {
+    $(".inspirationsCards").children("div").each(function() {
+        if ($(this).hasClass("tribute")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#twitch").click(function() {
+    $(".inspirationsCards").children("div").each(function() {
+        if ($(this).hasClass("twitch")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
