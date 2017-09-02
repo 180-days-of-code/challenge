dc05daa59784227002a955d12579beec37119293
Ritik <patniritik@gmail.com>
2017-09-02 10:20:46 +0530

    feat:added hire us page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(".button-collapse").sideNav();
 $(".dropdown-button").dropdown();
+// Project Page
 $("#all").click(function() {
     $(".inspirationsCards").children("div").each(function() {
         $(this).css("display", "");
     });
 });
-
 $("#portfolio").click(function() {
     $(".inspirationsCards").children("div").each(function() {
         if ($(this).hasClass("portfolio")) {
@@ -33,3 +33,74 @@ $("#twitch").click(function() {
         }
     });
 });
+// Learning Page
+$("#all-learning").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        $(this).css("display", "");
+    });
+});
+$("#html").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("html")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#css").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("css")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#js").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("js")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#UI-UX").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("UI/UX")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#tools").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("tools")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#css-frameworks").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("css-frameworks")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+$("#icons").click(function() {
+    $(".learning-links").children("ul").each(function() {
+        if ($(this).hasClass("icons")) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+});
+
+$('#textarea1').trigger('autoresize');
