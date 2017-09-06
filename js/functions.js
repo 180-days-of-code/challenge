68f9a7704e98321dcdec91790102821561c21749
Ritik <patniritik@gmail.com>
2017-09-06 19:15:13 +0530

    fix:minor fixes

--- js/functions.js
+++ js/functions.js
@@ -17,31 +17,8 @@ function getQuote() {
         var currentQuote = a[0].content;
         var quoteAuthor = a[0].title;
         console.log(currentQuote, quoteAuthor);
-        $('.quote-start').html("<i class='fa fa-quote-left green-text text-lighten-2 fa-2x'></i>");
-        $('.quote-end').html("<i class='fa fa-quote-right green-text text-lighten-2 fa-2x'></i>")
-
         $('.quote').html(currentQuote);
         $('.quote-author').html("-" + quoteAuthor);
-        $('.tweet-it').attr('href', 'https://twitter.com/intent/tweet?&text=' + '"' + $(".quote-message").text() + '"').attr('target', '_blank');
-        console.log($(".quote-message").text);
     });
 }
 getQuote();
-/*$(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({'padding-right':scrollWidth});
-}).resize();
-
-var teams =["Wildcards","The Coders United","JAVASK","Web Pros"];
-
-var table = document.createElement('table');
-document.getElementById('container').appendChild(table);
-for (row = 0; row < teams.length; row++) {
-    tr = document.createElement('tr');
-    table.appendChild(tr);
-    for (cell = 0; cell < 5; cell++) {
-        td = document.createElement('td');
-        tr.appendChild(td);
-        td.innerHTML = "text";
-    }
-}*/
